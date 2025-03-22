7f404101045fae971648d03972d3a1f58e719e26
rogerfernandez23 <rogerio.fernandes2212@gmail.com>
2025-03-22 17:21:01 -0300

    feat: new method of saving to spreadsheet

--- database/google_sheets.py
+++ database/google_sheets.py
@@ -1,20 +1,20 @@
 import gspread
 from config import get_google_sheets_client
+from datetime import datetime
 
-def insert_to_sheets(data_all, sheet_name='Dados'):
+def insert_to_sheets(data_all, spreadsheet_name='Teste'):
     client = get_google_sheets_client()
+    spreadsheet = client.open(spreadsheet_name)
 
-    spreadsheet = client.open("Teste")
-    sheet = spreadsheet.worksheet(sheet_name)
+    sheet_name = datetime.now().strftime('Relatório_%Y%m%d')
+    
+    sheet = spreadsheet.add_worksheet(title=sheet_name, rows="100", cols="50")
 
     headers = list(data_all[0].keys())
     values = [list(item.values()) for item in data_all]
 
-    exists_headers = sheet.row_values(1)
-
-    if not exists_headers:
-        sheet.append_row(headers)
-    
+    sheet.append_row(headers)
     sheet.append_rows(values)
 
     print("✅ Dados inseridos na planilha com sucesso!")
+    return sheet_name
